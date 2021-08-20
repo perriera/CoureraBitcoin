f82d151699a413e4ec879a5a5bda018b56e674e2
Perry Anderson <perry.anderson@gmail.com>
2021-08-20 13:15:09 -0700

    Refactored

--- src/test/java/BitCoinsFor.java
+++ src/test/java/BitCoinsFor.java
@@ -1,6 +1,13 @@
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.SignatureException;
+
 public class BitCoinsFor {
-   
-    public Transaction getGenesiseTx() {
+
+	public Transaction getGenesiseTx() {
 		return genesiseTx;
 	}
 
@@ -11,7 +18,7 @@ public class BitCoinsFor {
 	private Transaction genesiseTx;
 	private UTXOPool pool;
 
-    public BitCoinsFor(SampleBitcoinPeople people) {
+	public BitCoinsFor(SampleBitcoinPeople people) {
 		genesiseTx = new Transaction();
 		genesiseTx.addOutput(10, people.scroogeKeypair.getPublic());
 		genesiseTx.finalize();
@@ -19,6 +26,14 @@ public class BitCoinsFor {
 		UTXO utxo = new UTXO(genesiseTx.getHash(), 0);
 		pool.addUTXO(utxo, genesiseTx.getOutput(0));
 
+	}
+
+	public byte[] signMessage(PrivateKey sk, byte[] message)
+			throws NoSuchAlgorithmException, NoSuchProviderException, SignatureException, InvalidKeyException {
+		Signature sig = Signature.getInstance("SHA1withDSA", "SUN");
+		sig.initSign(sk);
+		sig.update(message);
+		return sig.sign();
 	}
 
 }
