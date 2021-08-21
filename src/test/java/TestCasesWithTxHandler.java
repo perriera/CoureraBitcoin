29b47ca1f3912f1e124357f4cee33c9122af53ec
Perry Anderson <perry.anderson@gmail.com>
2021-08-21 16:16:12 -0700

    Added test case

--- src/test/java/TestCasesWithTxHandler.java
+++ src/test/java/TestCasesWithTxHandler.java
@@ -17,30 +17,33 @@ public class TestCasesWithTxHandler extends TestCases {
 	public void testMaxFeeTransferUNEQUAL() throws Exception {
 		// Scrooge transfer 4 coins to Alice, 6 coins to bob, no transaction fee
 		TransactionInterface tx1 = new Transaction();
-		tx1 = authority.addCoinForSale(tx1, bitcoins.getGenesiseTx(), 0);
 		// tx1.addInput(bitcoins.getGenesiseTx().getHash(), 0);
-		tx1 = authority.addBuyer(tx1,4,authority.getAlice());
+		tx1 = authority.addCoinForSale(tx1, bitcoins.getGenesiseTx(), 0);
 		//tx1.addOutput(4, people.getAlice().getPublicKey());
-		tx1 = authority.addBuyer(tx1,6,authority.getBob());
+		tx1 = authority.addBuyer(tx1,4,authority.getAlice());
 		//tx1.addOutput(6, authority.getBob().getPublicKey());
-		tx1 = authority.authorizeSale(tx1,authority.getCreator(),0);
+		tx1 = authority.addBuyer(tx1,6,authority.getBob());
 		// byte[] sig1 = authority.signMessage(authority.getCreator().getPrivateKey(), tx1.getRawDataToSign(0));
 		// tx1.addSignature(sig1, 0);
 		// tx1.finalize();
+		tx1 = authority.authorizeSale(tx1,authority.getCreator(),0);
 
 		// Alice transfer 3.4 to mike, transaction fee is 4-3.4=0.6
-		Transaction tx2 = new Transaction();
-		tx2.addInput(tx1.getHash(), 0);
-		tx2.addOutput(3.4, authority.getMike().getPublicKey());
-		byte[] sig = authority.signMessage(authority.getAlice().getPrivateKey(), tx2.getRawDataToSign(0));
-		tx2.addSignature(sig, 0);
-		tx2.finalize();
+		TransactionInterface tx2 = new Transaction();
+		// tx2.addInput(tx1.getHash(), 0);
+		tx2 = authority.addCoinForSale(tx2, tx1, 0);
+		// tx2.addOutput(3.4, authority.getMike().getPublicKey());
+		tx2 = authority.addBuyer(tx2,3.4,authority.getMike());
+		// byte[] sig = authority.signMessage(authority.getAlice().getPrivateKey(), tx2.getRawDataToSign(0));
+		// tx2.addSignature(sig, 0);
+		// tx2.finalize();
+		tx2 = authority.authorizeSale(tx2,authority.getAlice(),0);
 
 		// Bob transfer 5.5 to mike, transaction fee is 5-5.5=0.5
 		Transaction tx3 = new Transaction();
 		tx3.addInput(tx1.getHash(), 1);
 		tx3.addOutput(5.5, authority.getMike().getPublicKey());
-		sig = authority.signMessage(authority.getBob().getPrivateKey(), tx3.getRawDataToSign(0));
+		byte[] sig = authority.signMessage(authority.getBob().getPrivateKey(), tx3.getRawDataToSign(0));
 		tx3.addSignature(sig, 0);
 		tx3.finalize();
 
